09bc74395baf8f0c118bd3be9db429e8bdcfa2f7
du0ks <73953584+du0ks@users.noreply.github.com>
2021-12-25 19:13:41 +0300

    Create problem281a.cpp

---
+++ codeforces/problem281a.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+ 
+int main(){
+	string s;
+	cin>>s;	
+	if(s[0]>=97 && s[0]<=122){
+	   s[0]-=32;
+	   cout<<s;
+	   	}
+	else 
+		cout<<s;	  
+	
+	return 0;
+}
+ 
